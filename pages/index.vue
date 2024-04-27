90ec2be6c4ef1ea21224e050d47ef0c3685cbe95
aceeedev <cyberandrew03@gmail.com>
2024-04-26 23:21:12 -0700

    Started index
    Also added styles.css, fonts, and 404 page

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1>I'm <span class="highlight">Andrew</span></h1>
+    <p>
+        I'm committed to fostering collaboration, communication, and the opportunity to 
+        learn from others! Please feel free to explore my work below or reach out to 
+        me here!
+    </p>
+</template>
