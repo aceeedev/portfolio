1db92e97229526fa42f8afc161a5bf08073776b0
aceeedev <cyberandrew03@gmail.com>
2024-05-08 22:12:53 -0700

    Started grid and mockup
    Also added icons

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,42 @@
 <template>
-    <h1>I'm <span class="highlight">Andrew</span></h1>
-    <p>
-        I'm committed to fostering collaboration, communication, and the opportunity to 
-        learn from others! Please feel free to explore my work below or reach out to 
-        me here!
-    </p>
+    <div class="container">
+        <div class="mockup">
+            <p>Andrew Collins</p>
+
+            <img />
+
+            <p></p>
+            <p></p>
+
+            <div class="center-align" style="gap: 10px;">
+                <img src="~assets/icons/previous_button.svg"/>
+                <img src="~assets/icons/pause_button.svg"/>
+                <img src="~assets/icons/next_button.svg"/>
+            </div>
+
+            <div>
+
+            </div>
+
+        </div>
+        <div style="grid-column: 2;">
+            <h1>I'm <span class="highlight">Andrew</span></h1>
+            <p>
+                I'm committed to fostering collaboration, communication, and the opportunity to 
+                learn from others! Please feel free to explore my work below or reach out to 
+                me here!
+            </p>
+
+            <h1>About Me</h1>
+            <p>
+                Hello my name is Andrew and I am a driven computer science and engineering student at Santa Clara University. Confident in my ability to thrive in collaborative environments and excited to learn new skills, I am eager to explore diverse opportunities and industries with a shared emphasis on technology, informing my professional career along the way. 
+                In my spare time, I’ll be found hiking vast lands, finding hidden indie music, or lost in a book.
+            </p>
+            <p>
+                Hello my name is Andrew and I am a driven computer science and engineering student at Santa Clara University. Confident in my ability to thrive in collaborative environments and excited to learn new skills, I am eager to explore diverse opportunities and industries with a shared emphasis on technology, informing my professional career along the way. 
+                In my spare time, I’ll be found hiking vast lands, finding hidden indie music, or lost in a book.
+            </p>
+
+        </div>
+    </div>
 </template>
