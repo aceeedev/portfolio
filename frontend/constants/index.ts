4289dd65a3dd058bb2ab4ab72c94c2ee1ef6b792
aceeedev <cyberandrew03@gmail.com>
2024-06-03 10:55:48 -0700

    Updated project and experience descriptions
    Also updated resume

--- frontend/constants/index.ts
+++ frontend/constants/index.ts
@@ -74,42 +74,42 @@ export const SECTIONS: Array<Section> = [
 export const PROJECTS: Array<Project> = [
     {
         'title': 'Toki Alarm',
-        'description': 'An alarm clock app for iOS with 950+ users that requires the user to solve a puzzle to turn off the alarm. Alongside the app, I created a business plan, marketing plan, and financials which resulted in a successful business.',
+        'description': 'Published full-stack puzzle alarm clock app for iOS with 1000+ users integrated with Apple\'s Game Center. Designed as a profitable solution to better waking up in the morning',
         'image': '/images/projects/toki.png',
         'links': ['https://apps.apple.com/us/app/toki-alarm-puzzle-alarm-clock/id1615680878', 'https://github.com/aceeedev/toki'],
         'tags': ['iOS', 'Flutter', 'SQLite'],
     },
     {
         'title': 'Brickdle.com',
-        'description': 'Parsed catalog of 19,000+ LEGO sets to create a daily Wordle-like game where users guess number of LEGO\'s in the picture. Over 2,000 plays in 37 countries in the first month!',
+        'description': 'Parsed 17000+ LEGO sets to create Wordle-like game where users guess number of LEGO\'s in images. Acquired 2.5K+ plays and 900+ visits across 37 countries in first month',
         'image': '/images/projects/brickdle.png',
         'links': [{'link': 'https://brickdle.com', 'name': 'Website'}, 'https://github.com/aceeedev/brickdle'],
         'tags': ['Web', 'Flutter', 'Firebase Analytics'],
     },
     {
         'title': 'Fist Bump',
-        'description': 'Winner of 3rd Place in Hack for Humanity 2024 hackathon. Developed full-stack website and API to connect students with on-campus events with custom calendar view.',
+        'description': 'Won 3rd Place in Hack for Humanity 2024 hackathon. Engineered full-stack website and API to connect students with on-campus events with custom calendar view',
         'image': '/images/projects/fist_bump.png',
         'links': ['https://devpost.com/software/fist-bump-wr63c8', 'https://github.com/LeoATX/humanityhack2024'],
         'tags': ['Web', 'Vue.js', 'REST API', 'Dart Shelf', 'NoSQL'],
     },
     {
         'title': 'Park, Bus, and Jam',
-        'description': '3rd place winner in the INRIX Hack 2023. Wrote full-stack transportation app invented to find most optimal parking and public transportation for concerts.',
+        'description': 'Won 3rd Place in INRIX Hack 2023 hackathon. Wrote full-stack transportation app invented to find most optimal parking and public transportation for concerts',
         'image': '/images/projects/pbj.png',
         'links': ['https://devpost.com/software/app-h2mex7', 'https://github.com/aceeedev/inrix-hack-23/tree/main'],
         'tags': ['Mobile App', 'Flutter', 'NoSQL', 'Flask', 'Python'],
     },
     {
         'title': 'Your Pitch',
-        'description': 'Won 3rd place in the Santa Clara University ACM Winter Challenge 2022. Developing a daily-curated music mobile app with a custom album cover calendar for iOS and Android. Created a custom-made Spotify Web API Wrapper with OAUTH 2.0. Additionally implemented a website for Your Pitch.',
+        'description': 'Won 3rd place in SCU ACM Winter Challenge 2022. Developed daily-curated music mobile app with custom Spotify Web API Wrapper and OAuth 2.0 PKCE Flow',
         'image': '/images/projects/your_pitch.jpg',
         'links': ['https://github.com/aceeedev/daily-spotify'],
         'tags': ['Mobile App', 'Flutter', 'OAuth 2.0', 'API Wrapper', 'NoSQL', 'Web'],
     },
     {
         'title': 'NestNotification',
-        'description': '4th place winner of INRIX Hack 2022. Created a full-stack location-sharing mobile app for the safety of adolescents and young adults.',
+        'description': 'Won 4th place in INRIX Hack 2022 hackathon. Created a full-stack location-sharing mobile app for the safety of adolescents and young adults.',
         'image': '/images/projects/nest_notifications.png',
         'links': ['https://devpost.com/software/nestnotifications', 'https://github.com/aceeedev/INRIX-Hack-22'],
         'tags': ['Mobile', 'Flutter', 'Flask', 'REST API', 'SQLite'],
@@ -126,8 +126,8 @@ export const PROJECTS: Array<Project> = [
 export const EXPERIENCES: Array<{main: Experience; journey: Array<Experience>;}> = [
     {
         'main': {
-            'title': 'SCU HCI Lab, Academic Researcher',
-            'description': 'Researching influence of tone and rhetoric using Valence-Arousal Model to improve UX of screen time messages on iOS. Collaborating with team of 5 through Agile and built with Swift.',
+            'title': 'SCU HCI Lab, Software Developer',
+            'description': 'Created custom data analysis tools with Python and Pandas in a collaborative Jupyter environment to investigate redesigning screen time management tools for digital wellbeing. Developed iOS app in Swift to be used to deploy for live research study utilizing iOS screen time APIs. Collaborated and led a team of 6 through Agile system on Monday.com and Trello',
             'image': '/images/experiences/scu_hci_logo.jpeg',
             'date': 'Jan 2023 - Present',
             'links': [{'link': 'https://scuhci.com/', 'name': 'Website'}],
@@ -137,7 +137,7 @@ export const EXPERIENCES: Array<{main: Experience; journey: Array<Experience>;}>
     {
         'main': {
             'title': 'SCU ACM, President',
-            'description': 'Responsible for managing 3 student-run hackathons (Hack for Humanity, INRIX Hack, and Roblox Hack) with 300+ participants each. Instructed technical workshops on Python/Flask and OpenAI to 200+ students.',
+            'description': 'Managed and ran 3 student-run hackathons (Hack for Humanity, INRIX Hack, and Roblox Hack) with 300+ participants each to give students valuable industry skills and opportunities. Instructed technical workshops on Python/Flask, Fullstack development, personal projects, and OpenAI to 200+ students to teach importance of going beyond academia',
             'image': '/images/experiences/acm_logo.png',
             'date': 'Oct 2022 - Present',
             'links': [{'link': 'https://scuacm.com', 'name': 'Website'}, {'link': 'https://hackforhumanity.io', 'name': 'Hack for Humanity'}],
@@ -145,21 +145,21 @@ export const EXPERIENCES: Array<{main: Experience; journey: Array<Experience>;}>
         'journey': [
             {
                 'title': 'SCU ACM, President',
-                'description': 'Leader stuff',
+                'description': '',
                 'image': '/images/experiences/acm_logo.png',
                 'date': 'Apr 2024 - Present',
                 'links': [{'link': 'https://scuacm.com', 'name': 'Website'}],
             },  
             {
                 'title': 'SCU ACM, Hackathon Coordinator',
-                'description': 'Responsible for managing 3 student-run hackathons (Hack for Humanity, INRIX Hack, and Roblox Hack) with 300+ participants each',
+                'description': 'Managed and ran 3 student-run hackathons (Hack for Humanity, INRIX Hack, and Roblox Hack) with 300+ participants each to give students valuable industry skills and opportunities',
                 'image': '/images/experiences/acm_hackathon.jpg',
                 'date': 'Apr 2023 - Apr 2024',
                 'links': [{'link': 'https://hackforhumanity.io', 'name': 'Hack for Humanity'}, {'link': 'https://inrix.scuacm.com', 'name': 'INRIX Hack'}, {'link': 'https://roblox.scuacm.com', 'name': 'Roblox Hack'}],
             }, 
             {
                 'title': 'SCU ACM, Underclassmen Representative',
-                'description': 'Instructed technical workshops on Python/Flask and OpenAI to 200+ students. In charge of engaging the community and exposing the club to more underclassmen.',
+                'description': 'Instructed technical workshops on Python/Flask, Fullstack development, personal projects, and OpenAI to 200+ students to teach importance of going beyond academia',
                 'image': '/images/experiences/acm_underclassmen.jpg',
                 'date': 'Oct 2022 - Apr 2023',
                 'links': [],
@@ -169,7 +169,7 @@ export const EXPERIENCES: Array<{main: Experience; journey: Array<Experience>;}>
     {
         'main': {
             'title': 'SCU Video Game Design Club, Vice President',
-            'description': 'Awarded Santa Clara University\'s Provisional Student Organization of the Year for fostering an interdisciplinary community of 125+ members.',
+            'description': 'Oversaw all technologies including GitHub (git project manager), Unity (C#), and web design in a collaborative space to encourage leadership and innovation. Awarded Santa Clara University’s Provisional Student Organization of the Year for fostering a creative space for expression for an interdisciplinary community of 125+ members',
             'image': '/images/experiences/vgdc_vp.png',
             'date': 'Dec 2022 - Present',
             'links': [],
@@ -184,7 +184,7 @@ export const EXPERIENCES: Array<{main: Experience; journey: Array<Experience>;}>
             }, 
             {
                 'title': 'SCU Video Game Design Club, C.T.O.',
-                'description': 'In charge of overseeing all technologies including GitHub (git project manager) and Unity (C#). Awarded Santa Clara University\'s Provisional Student Organization of the Year for fostering an interdisciplinary community of 125+ members.',
+                'description': 'Oversaw all technologies including GitHub (git project manager), Unity (C#), and web design in a collaborative space to encourage leadership and innovation. Awarded Santa Clara University’s Provisional Student Organization of the Year for fostering a creative space for expression for an interdisciplinary community of 125+ members',
                 'image': '/images/experiences/vgdc_cto.png',
                 'date': 'Dec 2022 - Sept 2024',
                 'links': [],
@@ -193,8 +193,8 @@ export const EXPERIENCES: Array<{main: Experience; journey: Array<Experience>;}>
     },
     {
         'main': {
-            'title': 'NexStream Technical Education, Project Lead',
-            'description': 'Coordinated team of students to create online virtual classroom web app using WebGL, Unity, and dedicated servers. Managed project vision, delegated tasks through a Scrum model, and handled documentation to keep team productive.',
+            'title': 'NexStream Technical Education, Technical Project Manager',
+            'description': 'Coordinated team of students to create online virtual classroom web app (HTML, Unity, Linux virtual machines) to engage and connect students during the pandemic academically. Managed project vision, delegated tasks through a Scrum model, and handled documentation to keep team productive and meet milestones',
             'image': '/images/experiences/nexstream.jpg',
             'date': 'Aug 2021 - Dec 2021',
             'links': [],
